3f7ee5b88e26abb79165563731160d3868c28e7b
Kuro <KuroMa@gmail.com>
2025-02-08 15:36:45 +0700

    Init source

---
+++ src/main/java/com/kuro/coffechain/entity/User.java
@@ -0,0 +1,31 @@
+package com.kuro.coffechain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
